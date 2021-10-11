e4047b9dec161dbd564a5b078fdac4126689d398
Daniel E. Gonzalez <dgonzalez80@gmail.com>
2021-10-11 10:15:54 -0500

    ajustes instructivos

--- R/n0.R
+++ R/n0.R
@@ -2,7 +2,6 @@
 #' @description Función que corrigue el tamaño de la muestra cuando n/N>0.05
 #' @param n0 valor del tamaño de la muestra inicial
 #' @param N valor del tamaño de la poblacion
-#' @param e valor del error de muestreo (diferencia entre la media muestral y el parámetro)
 #' @return n0 tamaño de muestra ajustado
 #' @export n0
 #' @examples  n0(385,500)
